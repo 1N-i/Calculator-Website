b284873bf432671f5131423115ac66e6a086f708
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-03-30 19:51:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+function ONCE() {
+    
+}
+ 
+function saveMemory() {
+    
+}
+
+function saveNegative() {
+    
+}
+
+function savePositive() {
+    
+}
+
+function OFF() {
+
+}
+
+function rootSquare() {
+    
+}
+
+function percentage() {
+    
+}
+
+function divide() {
+    
+}
+
+function add7() {
+
+}
+
+function add8() {
+    
+}
+
+function add9() {
+    
+}
+
+function multiply() {
+    
+}
+
+function add4() {
+
+}
+
+function add5() {
+    
+}
+
+function add6() {
+    
+}
+
+function subtract() {
+    
+}
+
+function add1() {
+
+}
+
+function add2() {
+    
+}
+
+function add3() {
+    
+}
+
+function plus() {
+    
+}
+
+function add0() {
+    
+}
+
+function addDot() {
+    
+}
+
+function seeResult() {
+    
+}
